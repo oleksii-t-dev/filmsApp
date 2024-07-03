de268cfb7f984330f666bf62f5f9553119f9b43b
Alex <atlas17891@gmail.com>
2024-07-03 13:42:44 +0300

    fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,15 @@
 import { useState, memo } from 'react';
-import { useSelector } from 'react-redux';
 import { Box, Grid, IconButton, Paper } from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import logo from '../assets/film-camera.svg';
 import { LoginModal } from './modal_windows/login_modal';
 import { TokenModal } from './modal_windows/token_modal';
-import { RootState } from '../store/store';
 
 export const Header = memo(function Header({ text }: { text: string }) {
-  const TOKEN = useSelector((state: RootState) => state.userReducer.token);
 
   const [openLogin, setOpenLogin] = useState<boolean>(false);
   const [openTokenModal, setOpenTokenModal] = useState<boolean>(false);
 
-  const handleClickOpenLogin: () => void = () => {
-    if (TOKEN === '') return setOpenLogin(true);
-    handleClickOpenTokenModal();
-  };
   const handleClickOpenTokenModal: () => void = () => {
     setOpenTokenModal(true);
   };
